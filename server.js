aa3e5a2e4a191afdb44565b2c4d64e0c77d2c68a
Md. Shahriar Anwar <anwarshahriar@gmail.com>
2017-01-07 15:57:46 +0000

    Adds body-parser

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var request = require("request");
+var bodyParser = require("body-parser");
 var URL = require("url");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/api/imagesearch/*', function (req, res) {
-    res.send(URL.parse(req.url));
+    res.send({offset: req.query.offset});
 });
 
 app.listen(PORT, function () {
